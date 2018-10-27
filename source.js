b43102687d9120777b7cb6bd2d8c220621ca0d6b
Andrew Taylor <andrew@andrewt.net>
2018-10-27 23:40:12 +0100

    Autocommit

--- source.js
+++ source.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', e => {
 
 function aaa(a) { return document.getElementById(a); }
 
-const road = $('road');
+const road = document.getElementById('road'),
+	keyLogger = document.getElementById('keylogger');
 
-console.log('key', aaa('keylogger'));
-aaa('keylogger').focus();
-aaa('keylogger').addEventListener('keypress', e => {
+console.log('key', keyLogger);
+keyLogger.focus();
+keyLogger.addEventListener('keypress', e => {
 	console.log('key', e);
 });
 

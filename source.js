3c7d504bcefa9c4131824eeb81316855090ee52d
Andrew Taylor <andrew@andrewt.net>
2018-10-27 23:36:41 +0100

    Autocommit

--- source.js
+++ source.js
@@ -2,6 +2,7 @@ function $(_) { return document.getElementById(_); }
 
 const road = $('road');
 
+console.log('key', $('keylogger'));
 $('keylogger').focus();
 $('keylogger').addEventListener('keypress', e => {
 	console.log('key', e);
